7fff941ce4f29fa7a2152332b153fe937fc1c405
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-10-24 21:59:29 +0500

    Add project files.

---
+++ ADO.NET/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.SqlClient;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        using (SqlConnection connect = new SqlConnection())
+        {
+            connect.ConnectionString = "Server = (localdb)\\MSSQLLocalDB; Database = LessonDb; Trusted_Connection = True;";
+            connect.Open();
+
+            string query = "Select firstname from Persons";
+            SqlCommand cmd = new SqlCommand(query, connect);
+
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader["firstname"]);
+                }
+            }
+
+        }
+    }
+}
+
+
+
+
