2709fc1f8d2427733a9b1fe55a1cd04149fe33cb
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-10-25 21:16:13 +0500

    udpate program class

--- ADO.NET/Program.cs
+++ ADO.NET/Program.cs
@@ -1,29 +1,31 @@
-﻿using System.Data.SqlClient;
+﻿//using System.Data.SqlClient;
 
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        using (SqlConnection connect = new SqlConnection())
-        {
-            connect.ConnectionString = "Server = (localdb)\\MSSQLLocalDB; Database = LessonDb; Trusted_Connection = True;";
-            connect.Open();
+//public class Program
+//{
+//    public static void Main(string[] args)
+//    {
+//        using (SqlConnection connect = new SqlConnection())
+//        {
+//            connect.ConnectionString = "Server = (localdb)\\MSSQLLocalDB; Database = LessonDb; Trusted_Connection = True;";
+//            connect.Open();
 
-            string query = "Select firstname from Persons";
-            SqlCommand cmd = new SqlCommand(query, connect);
+//            string query = "Select firstname from Persons";
+//            SqlCommand cmd = new SqlCommand(query, connect);
 
-            using (SqlDataReader reader = cmd.ExecuteReader())
-            {
-                while (reader.Read())
-                {
-                    Console.WriteLine(reader["firstname"]);
-                }
-            }
+//            using (SqlDataReader reader = cmd.ExecuteReader())
+//            {
+//                while (reader.Read())
+//                {
+//                    Console.WriteLine(reader["firstname"]);
+//                }
+//            }
 
-        }
-    }
-}
+//        }
+//    }
+//}
 
+using ADO.NET;
 
+Repository repository = new Repository();
 
-
+repository.GetByIdAsync(2);
