5ae9c9a4069bccd5b5718679b06e922ac749eaac
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-10-25 21:16:29 +0500

    created repository

---
+++ ADO.NET/Repository.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.SqlClient;
+
+namespace ADO.NET
+{
+    public class Repository : IRepository<Users>
+    {
+        private readonly string _connectionString;
+
+        public Repository()
+        {
+            _connectionString = "Server = (localdb)\\MSSQLLocalDB; Database = LessonDb; Trusted_Connection = True;";
+        }
+        public void GetByIdAsync(int id)
+        {
+            using (SqlConnection connect = new SqlConnection())
+            {
+                connect.ConnectionString = _connectionString;
+                connect.Open();
+
+                string query = $"Select * from Others Where Id = {id}";
+                SqlCommand cmd = new SqlCommand(query, connect);
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["Id"]}  {reader["firstname"]}   {reader["lastname"]}    {reader["age"]}");
+                    }
+                }
+
+            }
+        }
+    }
+}
+
