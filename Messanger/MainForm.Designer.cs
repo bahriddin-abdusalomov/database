97bd3896c9eaaa0c46be192ded7dcabd46c786d2
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-10-25 21:18:37 +0500

    messanger project

---
+++ Messanger/MainForm.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Messanger
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            firstNameTb = new TextBox();
+            emailTb = new TextBox();
+            phoneNumberTb = new TextBox();
+            lastNameTb = new TextBox();
+            registerBtn = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(54, 58);
+            label1.Name = "label1";
+            label1.Size = new Size(100, 25);
+            label1.TabIndex = 0;
+            label1.Text = "First name";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(54, 220);
+            label2.Name = "label2";
+            label2.Size = new Size(58, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Email";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(54, 140);
+            label3.Name = "label3";
+            label3.Size = new Size(98, 25);
+            label3.TabIndex = 2;
+            label3.Text = "Last name";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(54, 305);
+            label4.Name = "label4";
+            label4.Size = new Size(137, 25);
+            label4.TabIndex = 3;
+            label4.Text = "Phone number";
+            // 
+            // firstNameTb
+            // 
+            firstNameTb.Location = new Point(73, 97);
+            firstNameTb.Name = "firstNameTb";
+            firstNameTb.Size = new Size(298, 27);
+            firstNameTb.TabIndex = 4;
+            // 
+            // emailTb
+            // 
+            emailTb.Location = new Point(73, 260);
+            emailTb.Name = "emailTb";
+            emailTb.Size = new Size(298, 27);
+            emailTb.TabIndex = 5;
+            // 
+            // phoneNumberTb
+            // 
+            phoneNumberTb.Location = new Point(73, 344);
+            phoneNumberTb.Name = "phoneNumberTb";
+            phoneNumberTb.Size = new Size(298, 27);
+            phoneNumberTb.TabIndex = 6;
+            // 
+            // lastNameTb
+            // 
+            lastNameTb.Location = new Point(73, 180);
+            lastNameTb.Name = "lastNameTb";
+            lastNameTb.Size = new Size(298, 27);
+            lastNameTb.TabIndex = 7;
+            // 
+            // registerBtn
+            // 
+            registerBtn.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            registerBtn.Location = new Point(98, 401);
+            registerBtn.Name = "registerBtn";
+            registerBtn.Size = new Size(246, 49);
+            registerBtn.TabIndex = 8;
+            registerBtn.Text = "Register";
+            registerBtn.UseVisualStyleBackColor = true;
+            registerBtn.Click += registerBtn_Click;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(469, 482);
+            Controls.Add(registerBtn);
+            Controls.Add(lastNameTb);
+            Controls.Add(phoneNumberTb);
+            Controls.Add(emailTb);
+            Controls.Add(firstNameTb);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "MainForm";
+            Text = "Messanger";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox firstNameTb;
+        private TextBox emailTb;
+        private TextBox phoneNumberTb;
+        private TextBox lastNameTb;
+        private Button registerBtn;
+    }
+}
---
+++ Messanger/MessagePage.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Messanger
+{
+    partial class MessagePage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            allMessageTb = new TextBox();
+            toWhomTb = new TextBox();
+            toWhomBtn = new Button();
+            textBox1 = new TextBox();
+            getMessageBtn = new Button();
+            sendBtn = new Button();
+            SuspendLayout();
+            // 
+            // allMessageTb
+            // 
+            allMessageTb.Location = new Point(55, 72);
+            allMessageTb.Multiline = true;
+            allMessageTb.Name = "allMessageTb";
+            allMessageTb.Size = new Size(705, 316);
+            allMessageTb.TabIndex = 0;
+            // 
+            // toWhomTb
+            // 
+            toWhomTb.Location = new Point(55, 20);
+            toWhomTb.Name = "toWhomTb";
+            toWhomTb.Size = new Size(245, 27);
+            toWhomTb.TabIndex = 1;
+            // 
+            // toWhomBtn
+            // 
+            toWhomBtn.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            toWhomBtn.Location = new Point(352, 20);
+            toWhomBtn.Name = "toWhomBtn";
+            toWhomBtn.Size = new Size(136, 29);
+            toWhomBtn.TabIndex = 2;
+            toWhomBtn.Text = "To Whom";
+            toWhomBtn.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(55, 415);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(456, 27);
+            textBox1.TabIndex = 3;
+            // 
+            // getMessageBtn
+            // 
+            getMessageBtn.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            getMessageBtn.Location = new Point(652, 413);
+            getMessageBtn.Name = "getMessageBtn";
+            getMessageBtn.Size = new Size(108, 29);
+            getMessageBtn.TabIndex = 4;
+            getMessageBtn.Text = "Get";
+            getMessageBtn.UseVisualStyleBackColor = true;
+            // 
+            // sendBtn
+            // 
+            sendBtn.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            sendBtn.Location = new Point(531, 413);
+            sendBtn.Name = "sendBtn";
+            sendBtn.Size = new Size(104, 29);
+            sendBtn.TabIndex = 5;
+            sendBtn.Text = "Send";
+            sendBtn.UseVisualStyleBackColor = true;
+            // 
+            // MessagePage
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(815, 475);
+            Controls.Add(sendBtn);
+            Controls.Add(getMessageBtn);
+            Controls.Add(textBox1);
+            Controls.Add(toWhomBtn);
+            Controls.Add(toWhomTb);
+            Controls.Add(allMessageTb);
+            Name = "MessagePage";
+            Text = "MessagePage";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox allMessageTb;
+        private TextBox toWhomTb;
+        private Button toWhomBtn;
+        private TextBox textBox1;
+        private Button getMessageBtn;
+        private Button sendBtn;
+    }
+}
