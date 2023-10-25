13cd5f22b2e1ba923462bc31ede244cfbc1dcba8
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-10-25 21:16:44 +0500

    created user model

---
+++ ADO.NET/Users.cs
@@ -0,0 +1,9 @@
+﻿namespace ADO.NET;
+
+public class Users
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+}
