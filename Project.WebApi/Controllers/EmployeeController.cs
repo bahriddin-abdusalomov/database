262a58c507f6cf74562e7214f95bd418e6186646
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-10-26 23:50:37 +0500

    created employee controller

---
+++ Project.WebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Project.WebApi.Dtos;
+using Project.WebApi.Interfaces;
+using Project.WebApi.Models;
+
+namespace Project.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeRepository employeeRepository;
+
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            this.employeeRepository = employeeRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync([FromForm] EmployeeDto employee)
+            => Ok(await  employeeRepository.CreateAsync(employee));
+
+        [HttpDelete]
+        public async Task<IActionResult> DeepDeleteAsync([FromForm] int id)
+            => Ok(await employeeRepository.DeepDeletedAsync(id));
+
+        [HttpGet]
+        public async Task<IActionResult> GetByIdAsync([FromQuery] int id)
+            => Ok(await employeeRepository.GetAsync(id));
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAsync([FromForm] EmployeeDto employeeDto, int id)
+            => Ok(await employeeRepository.UpdateAsync(id, employeeDto));
+    }
+}
+    
