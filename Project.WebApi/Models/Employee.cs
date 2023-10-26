2c9c3fac3f337caf04a8099d7325613b41c2f99b
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-10-26 23:51:54 +0500

    created employee model

---
+++ Project.WebApi/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using Project.WebApi.Enums;
+
+namespace Project.WebApi.Models;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Email { get; set; }
+    public string Login { get ; set; }
+    public string Password { get; set; }
+    public Status Status { get; set; }
+    public Role Role { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime Updated { get; set; }
+    public DateTime Deleted { get; set; }
+}
