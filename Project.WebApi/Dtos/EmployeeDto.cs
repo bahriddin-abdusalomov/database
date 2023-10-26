4d4ae8d50383228d8e9ab103e4399a1f4300ba75
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-10-26 23:50:58 +0500

    created employee dto

---
+++ Project.WebApi/Dtos/EmployeeDto.cs
@@ -0,0 +1,14 @@
+﻿using Project.WebApi.Enums;
+
+namespace Project.WebApi.Dtos;
+
+public class EmployeeDto
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Email { get; set; }
+    public string Login { get; set; }
+    public string Password { get; set; }
+    public Status Status { get; set; }
+    public Role Role { get; set; }
+}
