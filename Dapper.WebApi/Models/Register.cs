b07a20fa165f5f7b242548ee6e05aebe58f52991
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-10-27 21:30:05 +0500

    created user model

---
+++ Dapper.WebApi/Models/Register.cs
@@ -0,0 +1,9 @@
+﻿namespace Dapper.WebApi.Models;
+
+public class Register
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+}
