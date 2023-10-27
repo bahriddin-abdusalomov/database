21e01607ec497e12e0a7be91ad039dce4c3bacb4
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-10-27 21:33:26 +0500

    created message model

---
+++ Messanger/Models/Message.cs
@@ -0,0 +1,11 @@
+﻿namespace Messanger.Models;
+
+public class Message
+{
+    public int Id { get; set; }
+    public string UserName { get; set; }
+    public string MyUserName { get; set; }
+    public string Messages { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime Updated { get; set;}
+}
