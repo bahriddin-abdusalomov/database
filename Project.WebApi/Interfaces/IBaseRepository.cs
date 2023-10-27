72ecc0a4d2e74b2e33e2552b475da7d853ff23d7
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-10-27 14:13:11 +0500

    update base repository

--- Project.WebApi/Interfaces/IBaseRepository.cs
+++ Project.WebApi/Interfaces/IBaseRepository.cs
@@ -1,11 +1,11 @@
 ﻿namespace Project.WebApi.Interfaces;
 
-public interface IBaseRepository<T> where T : class
+public interface IBaseRepository<TModel, TView>
 {
-    public Task<int> CreateAsync(T entity);
-    public Task<int> UpdateAsync(int id, T entity);
+    public Task<int> CreateAsync(TView entity);
+    public Task<int> UpdateAsync(int id, TView entity);
     public Task<int> DeleteAsync(int id);
     public Task<int> DeepDeletedAsync(int id);
-    public Task<T> GetAsync(int id);
-    public Task<List<T>> GetAllAsync();
+    public Task<TModel> GetAsync(int id);
+    public Task<List<TModel>> GetAllAsync();
 }
