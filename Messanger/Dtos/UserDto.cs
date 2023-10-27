9b27bf68ca2f2de282a67c7cbad3a30ed6e333b1
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-10-27 21:32:36 +0500

    created user dto

---
+++ Messanger/Dtos/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Messanger.Dtos;
+
+public class UserDto
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public string UserName { get; set; } = string.Empty;
+}
