21631ae236ce75d4356d59f287f3dc10a8486007
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-10-27 14:12:40 +0500

    update employee dto

--- Project.WebApi/Dtos/EmployeeDto.cs
+++ Project.WebApi/Dtos/EmployeeDto.cs
@@ -9,6 +9,5 @@ public class EmployeeDto
     public string Email { get; set; }
     public string Login { get; set; }
     public string Password { get; set; }
-    public Status Status { get; set; }
     public Role Role { get; set; }
 }
