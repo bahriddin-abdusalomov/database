d85498522b55b106ddcd735ad6e5bd3944d034e3
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-10-31 23:03:27 +0500

    search value from database

---
+++ SearchDb.WebApi/Controllers/SearchController.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SearchDb.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        private readonly string connectionString = WebApplication.CreateBuilder().Configuration.GetConnectionString("Connect");
+
+        [HttpGet]
+        public IActionResult SearchValue(string searchStr)
+        {
+            using ( SqlConnection connection = new SqlConnection(connectionString))
+            {
+                    string query = $"EXEC SearchProcedure {searchStr}";
+                    var result = connection.Query<Search>(query);
+                     return Ok(result);
+            }
+
+        }
+    }
+}
---
+++ SearchDb.WebApi/Search.cs
@@ -0,0 +1,8 @@
+﻿namespace SearchDb.WebApi;
+
+public class Search
+{
+    public string Id { get; set; }
+    public string ColumnName { get; set; }
+    public string ColumnValue { get; set; }
+}
