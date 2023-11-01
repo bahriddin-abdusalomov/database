7b05d426d9744ab7b5bfaa235c5c6e711eb6b397
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-11-01 17:55:28 +0500

    update search model

---
+++ SearchDb.WebApi/Models/Search.cs
@@ -0,0 +1,8 @@
+﻿namespace SearchDb.WebApi.Models;
+
+public class Search
+{
+    public string Id { get; set; }
+    public string ColumnName { get; set; }
+    public string ColumnValue { get; set; }
+}
