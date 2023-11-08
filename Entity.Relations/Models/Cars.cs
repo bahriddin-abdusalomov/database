4e6f4cb5dc79817916b50389d4980eec079fe8ec
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-11-08 21:55:54 +0500

    created car model

---
+++ Entity.Relations/Models/Cars.cs
@@ -0,0 +1,10 @@
+﻿namespace Entity.Relations.Models
+{
+    public class Cars
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<PersonCars> CarsPerson { get; set; }
+    }
+}
