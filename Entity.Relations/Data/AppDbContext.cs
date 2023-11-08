584be501a401d79db198838c13c80b0072cfeddd
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-11-08 21:55:25 +0500

    created appdbcontext class

---
+++ Entity.Relations/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Entity.Relations.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Entity.Relations.Data
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Cars> Cars { get; set; }
+        public virtual DbSet<Person> Persons { get; set; }
+        public virtual DbSet<PersonCars> PersonCars { get; set; }
+    }
+}
